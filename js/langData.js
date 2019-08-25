140e7dfe468699d51d84b77415c00f259b6b1769
Toni Kucic <tkucic@gmail.com>
2019-08-25 13:55:34 +0300

    Simplify website by deleting useless data

--- js/langData.js
+++ js/langData.js
@@ -1,58 +1,27 @@
 //Defines what text should be displayed for what object id
 var langData = {
     en: {
-        homeLink : "HOME",
         aboutLink : "ABOUT",
-        eduLink : "EDUCATION",
-        careerLink : "CAREER",
         projLink : "PROJECTS",
         coursesLink : "COURSES",
         docsLink : "DOCS",
-        contactLink : "CONTACT",
-        homeTitle : "INDUSTRIAL AUTOMATION PROFESSIONAL WITH FOCUS ON MARINE INDUSTRY",
-        areasOfExpertise : "AREAS OF EXPERTISE",
-        exp0 : "Marine Engineering",
-		exp1 : "Electrical Engineering",
-		exp2 : "Industrial automation",
-		exp3 : "PLC Programming",						
-		exp4 : "Python programming",
-		exp5 : "Documentation frameworks"
-
+        homeTitle : "HI, I AM TONI, PLC PROGRAMMER.",
+        homeText : "Read more about me below"
     },
     fi:{
-        homeLink : "KOTI",
         aboutLink : "MINUSTA",
-        eduLink : "KOULUTUS",
-        careerLink : "URANI",
         projLink : "PROJEKTIT",
         coursesLink : "KURSSIT",
         docsLink : "DOKUMENTIT",
-        contactLink : "YHTEYDET",
-        homeTitle : "MERITEOLLISUUDEN AUTOMAATIO AMMATILAINEN",
-        areasOfExpertise : "AMATTI ALUEET",
-        exp0 : "Meri-insinöörityö",
-		exp1 : "Sähkösuunnittelu",
-		exp2 : "Teollisuusautomaatio",
-		exp3 : "PLC Ohjelmointi",						
-		exp4 : "Python Ohjelmointi",
-		exp5 : "Dokumentatio-frameworks"
+        homeTitle : "MOI, MINÄ OLEN TONI, PLC KOODARI.",
+        homeText : "Lue lisää minusta alhaalla"
     },
     hr:{
-        homeLink : "POCETNA",
         aboutLink : "O MENI",
-        eduLink : "EDUKACIJA",
-        careerLink : "KARIJERA",
         projLink : "PROJEKTI",
         coursesLink : "KURSEVI",
         docsLink : "DOKUMENTI",
-        contactLink : "KONTAKT",
-        homeTitle : "PROFESIONALAC INDUSTRIJSKE AUTOMATIKE SA NAGLASKOM NA POMORSKU INDUSTRIJU",
-        areasOfExpertise : "PODRUCJA EKSPERTIZE",
-        exp0 : "Pomorsko inzinjerstvo",
-		exp1 : "Dizajn elektrickih sustava",
-		exp2 : "Industrijska automatika",
-		exp3 : "PLC programiranje",						
-		exp4 : "Python programiranje",
-		exp5 : "Dokumentacijski programi"
+        homeTitle : "POZDRAV, JA SAM TONI, PLC PROGRAMER.",
+        homeText : "Procitaj ispod vise o meni"
     }
 }
--- js/mainscript.js
+++ js/mainscript.js
@@ -8,48 +8,48 @@ function respNav() {
     }
 }
 
-// Get the element with id="defaultOpenEdu" and id="defaultOpenCarr" andclick on it
-document.getElementById("defaultOpenEdu").click();
-document.getElementById("defaultOpenCarr").click();
-function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+// // Get the element with id="defaultOpenEdu" and id="defaultOpenCarr" andclick on it
+// document.getElementById("defaultOpenEdu").click();
+// document.getElementById("defaultOpenCarr").click();
+// function openTab(evt, tabName) {
+//     // Declare all variables
+//     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+//     // Get all elements with class="tabcontent" and hide them
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+//     // Get all elements with class="tablinks" and remove the class "active"
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(tabName).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
 
-//Slideshow functionality
-var slideIndex = [1,1];
-/* Class the members of each slideshow group with different CSS classes */
-var slideId = ["slideshow1","slideshow2"] 
-showSlides(1, 0);
-showSlides(1, 1);
+// //Slideshow functionality
+// var slideIndex = [1,1];
+// /* Class the members of each slideshow group with different CSS classes */
+// var slideId = ["slideshow1","slideshow2"] 
+// showSlides(1, 0);
+// showSlides(1, 1);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
+// function plusSlides(n, no) {
+//   showSlides(slideIndex[no] += n, no);
+// }
 
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1} 
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  x[slideIndex[no]-1].style.display = "block"; 
-}
+// function showSlides(n, no) {
+//   var i;
+//   var x = document.getElementsByClassName(slideId[no]);
+//   if (n > x.length) {slideIndex[no] = 1} 
+//   if (n < 1) {slideIndex[no] = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none"; 
+//   }
+//   x[slideIndex[no]-1].style.display = "block"; 
+// }
